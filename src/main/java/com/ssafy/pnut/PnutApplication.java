9f59097a28d54355304e04ae260d85d8beaa8140
최재희 <withfavor98@naver.com>
2023-03-06 17:50:09 +0900

    setting

---
+++ src/main/java/com/ssafy/pnut/PnutApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.pnut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnutApplication.class, args);
+	}
+
+}
