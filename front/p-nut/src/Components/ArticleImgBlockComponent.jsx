296d1949aef00666ee609e10d786ea7110f89a04
vinitus <tlsdnrng@gmail.com>
2023-03-17 10:32:15 +0900

    [refactor] import React and props destructuring

--- front/p-nut/src/Components/ArticleImgBlockComponent.jsx
+++ front/p-nut/src/Components/ArticleImgBlockComponent.jsx
@@ -1,12 +1,16 @@
-const ArticleImgBlockComponent = (props) => {
+import React from "react";
+
+function ArticleImgBlockComponent(props) {
+  const { size, text } = props;
+
   return (
     <div
-      className={`bg-#F5F5F5 flex items-center justify-center flex-col grey-underbar ${props.size}`}
+      className={`bg-#F5F5F5 flex items-center justify-center flex-col grey-underbar ${size}`}
     >
       <img src="./assets/plus.png" alt="plus" />
-      <div className="mt-20 text-2xl">{props.text}</div>
+      <div className="mt-20 text-2xl">{text}</div>
     </div>
   );
-};
+}
 
 export default ArticleImgBlockComponent;
