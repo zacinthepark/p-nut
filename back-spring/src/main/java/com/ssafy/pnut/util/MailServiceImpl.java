21b68cc3079ea387f1537f3e937b75fb2ed4ac99
polarbearHam <gkagu12@naver.com>
2023-03-20 17:18:56 +0900

    [refactor] modify mail http form

--- back-spring/src/main/java/com/ssafy/pnut/util/MailServiceImpl.java
+++ back-spring/src/main/java/com/ssafy/pnut/util/MailServiceImpl.java
@@ -52,7 +52,7 @@ public class MailServiceImpl implements MailService {
             String content =
                     "안녕하세요 Pnut 입니다." + 	//html 형식으로 작성 !
                             "<br><br>" +
-                            "인증 번호는 " + authNum + "입니다." +
+                            "인증 번호는 "+"<h2>" + authNum + "</h2>" + " 입니다." +
                             "<br>" +
                             "해당 인증번호를 인증번호 확인란에 기입하여 주세요."; //이메일 내용 삽입
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "utf-8");
