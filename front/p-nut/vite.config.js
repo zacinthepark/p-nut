2ab430742132b2cb348abaff7d0135219bbd4884
vinitus <tlsdnrng@gmail.com>
2023-03-24 14:05:35 +0900

    [chore] add proxy server

--- front/p-nut/vite.config.js
+++ front/p-nut/vite.config.js
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/boards": {
+        target: "http://j8a704.p.ssafy.io:9090",
+      },
+    },
+  },
 });
