d426f7775c94c070eaaea342b8cda0a9d2b942a9
Ming0Kim <minofficial13@gmail.com>
2023-03-27 13:21:58 +0900

    [Feat] Create Search Recommend Page

--- front/p-nut/src/Components/RecipeThumbnailComponent.jsx
+++ front/p-nut/src/Components/RecipeThumbnailComponent.jsx
@@ -5,12 +5,14 @@ const RecipeThumbnailComponent = (props) => {
   return (
     <div className="">
       <img src={imgPath} alt="" />
-      <div>
-        <div>{title}</div>
-        <div>{kcal}</div>
+      <div className="my-10 flex items-end text-end space-x-5">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="bg-#FF6B6C/70 text-end text-white px-10 py-3 rounded-full">
+          {kcal} kcal
+        </p>
       </div>
-      <div>{mainIngredients}</div>
-      <div>{time}</div>
+      <div className="text-lg text-gray-700">주 재료 : {mainIngredients}</div>
+      <div className="text-lg text-gray-700">예상 조리시간 : {time}분</div>
     </div>
   );
 };
