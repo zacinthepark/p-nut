03cf57e75189baf11cd1a79ef78db7abd77df8ca
vinitus <tlsdnrng@gmail.com>
2023-03-27 12:58:50 +0900

    [feat] fetching response data

--- front/p-nut/src/Pages/ArticleDetailPage.jsx
+++ front/p-nut/src/Pages/ArticleDetailPage.jsx
@@ -1,27 +1,38 @@
+import axios from "axios";
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 import OrderBlockComponent from "../Components/OrderBlockComponent";
 
 const ArticleDetailPage = () => {
-  const author = "콩이";
-  const title = "태국식 시금치 덮밥";
-  const time = "30분 컷";
-  const quantity = "2인분";
-  const description =
-    "닭도리탕은 단백질이 풍부하여 영양 보충에 도움이 됩니다. 또한, 닭을 주된 재료로 하는 음식으로서 다이어트에도 효능이 있습니다.";
-  const ingredients =
-    "[재료] 구수한 사골육수 500ml(약 1/2팩), 김치 100g, 소시지 100g, 통조림햄 100g, 베이크드빈스 50g, 두부 1/4모(75g), 양파 1/4개(50g), 체다 치즈 1장, 대파 10g, 고춧가루 2큰술, 다진 마늘 1큰술";
-  const thumbnailImgPath = "/assets/article_thumbnail.png";
+  // content ingredients nickNAme quantity recipeSteps thumbnail_image_url time title visit
+  const data = useLoaderData();
+
+  const {
+    content,
+    ingredients,
+    nickName,
+    quantity,
+    recipeSteps,
+    thumbnailImageUrl,
+    time,
+    title,
+    visit,
+  } = data.data;
+  console.log(recipeSteps);
+  for (const value in recipeSteps) {
+    console.log(value, recipeSteps[value]);
+  }
   const profileImgPath = "/assets/Article_circle.png";
-  const cookingOrder = [
-    {
-      orderDescription: "두부, 통조림햄, 소시지는 먹기 좋은 크기로 썰어주세요.",
-      orderImgPath: "/assets/orderImg1.png",
-    },
-    {
-      orderDescription: "끓이세요.",
-      orderImgPath: "/assets/orderImg2.png",
-    },
-  ];
+  // const recipeSteps = [
+  //   {
+  //     orderDescription: "두부, 통조림햄, 소시지는 먹기 좋은 크기로 썰어주세요.",
+  //     orderImgPath: "/assets/orderImg1.png",
+  //   },
+  //   {
+  //     orderDescription: "끓이세요.",
+  //     orderImgPath: "/assets/orderImg2.png",
+  //   },
+  // ];
   const commentIdList = [1234, 12345, 123456];
   const commentIdListCnt = commentIdList.length;
   const favoriteCnt = 13;
@@ -31,14 +42,14 @@ const ArticleDetailPage = () => {
     <div className="flex w-1200 mx-auto flex-col">
       <div className="flex mb-85">
         <img
-          src={`${thumbnailImgPath}`}
+          src={`${thumbnailImageUrl}`}
           alt=""
           className="w-586 h-407 mx-7 my-auto"
         />
         <div className="w-600 pl-80">
           <div className="flex items-center mb-30">
             <img src={profileImgPath} alt="" className="w-51 h-51 mx-4" />
-            <div className="mx-15 text-27">{author}</div>
+            <div className="mx-15 text-27">{nickName}</div>
           </div>
           <div className="text-33 font-bold mb-32 h-37">{title}</div>
           <div className="flex items-center pb-32 grey-underbar">
@@ -47,7 +58,7 @@ const ArticleDetailPage = () => {
             </div>
             <div className="ml-27 text-26 font-semibold">{quantity}</div>
           </div>
-          <div className="my-26 text-22 font-medium">{description}</div>
+          <div className="my-26 text-22 font-medium">{content}</div>
           <div className="w-full flex place-content-between">
             <div className="text-22">
               댓글 {commentIdListCnt} 좋아요 {favoriteCnt}
@@ -66,12 +77,8 @@ const ArticleDetailPage = () => {
           {ingredients}
         </div>
       </div>
-      {cookingOrder.map((value, idx) => (
-        <OrderBlockComponent
-          imgPath={value.orderImgPath}
-          text={value.orderDescription}
-          idx={idx}
-        />
+      {Object.entries(recipeSteps).map(([key, value], idx) => (
+        <OrderBlockComponent imgPath={key} text={value} idx={idx} />
       ))}
       <div className="mt-150 flex items-center w-1200 mx-auto">
         <div className="text-37 font-bold">댓글</div>
@@ -84,3 +91,9 @@ const ArticleDetailPage = () => {
 };
 
 export default ArticleDetailPage;
+
+export async function loader({ params }) {
+  const res = await axios.get(`/boards/board/${params.articleId}`);
+  console.log(res);
+  return res;
+}
