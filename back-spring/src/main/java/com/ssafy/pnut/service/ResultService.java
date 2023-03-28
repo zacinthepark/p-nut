e1a2ba91a02c3db57b4e559fe4fbc911ea3a4374
최재희 <withfavor98@naver.com>
2023-03-28 21:13:27 +0900

    add : survey to save, delete

--- back-spring/src/main/java/com/ssafy/pnut/service/ResultService.java
+++ back-spring/src/main/java/com/ssafy/pnut/service/ResultService.java
@@ -1,4 +1,14 @@
 package com.ssafy.pnut.service;
 
+import com.ssafy.pnut.entity.User;
+import com.ssafy.pnut.entity.question;
+import com.ssafy.pnut.entity.result;
+
 public interface ResultService {
+
+    void save(result Result);
+
+    void deleteByQuestionIdAndUserEmail(question questionId, User userEmail);
+
+
 }
