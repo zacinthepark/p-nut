8cdf006d640b20380830566bc5a5240da4c2e6c8
vinitus <tlsdnrng@gmail.com>
2023-03-30 18:01:28 +0900

    [style] update style

--- front/p-nut/src/Pages/SurveyLayout.jsx
+++ front/p-nut/src/Pages/SurveyLayout.jsx
@@ -5,7 +5,9 @@ const SurveyLayout = () => {
   return (
     <div className="bg-#ECECEC w-screen h-screen flex items-center justify-center">
       <main className="w-1024 h-768 bg-prettywhite shadow-md">
-        <Outlet />
+        <div className="flex flex-col items-center justify-center h-full">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
