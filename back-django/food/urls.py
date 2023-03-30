c1211b31469d7ddd13b463017e5c5ac9aa80c3cd
한원석 <wonsoongnim@naver.com>
2023-03-30 15:53:02 +0900

    [chore] edit settings.py

--- back-django/food/urls.py
+++ back-django/food/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.foods),
+    path('', views.get_personal_food),
+    path('calc/', views.calc_weight),
     path('info/', views.get_single_food),
     path('search/', views.search_food),
     path('nutrient/', views.lack_of_nutrient),
