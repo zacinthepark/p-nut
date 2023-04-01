37f77ce966c5e32047970324d5310c4b07f497ee
ming0kim <minofficial13@gmail.com>
2023-04-02 02:24:53 +0900

    [Feat] survey-card-thumbnail-component surgery complete

--- front/p-nut/src/Components/SurveyCardThumbnailComponent.jsx
+++ front/p-nut/src/Components/SurveyCardThumbnailComponent.jsx
@@ -1,13 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Modal from "../UI/Modal";
+import axios from "axios";
+import foodTestAPI from "../api/foodTestAPI";
 
 const SurveyCardThumbnailComponent = (props) => {
   const { imgPath, foodTitle } = props;
 
+  const [youtubeData, setYoutubeData] = useState();
+  const [foodData, setFoodData] = useState(null);
+
+  // FoodTestAPI를 위한 userEmail 가져오기
+  const state = JSON.parse(localStorage.getItem("persist:root"));
+  const authentication = JSON.parse(state.auth);
+  const userEmail = authentication.authentication.email;
+
+  const openModal = (event) => {
+    event.stopPropagation();
+    axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${foodTitle}&type=video&videoDefinition=high&key=AIzaSyCI8t8M1ADPjcTTAuIOs3G2w-Nev9hXwRs`
+      )
+      .then((res) => {
+        setYoutubeData(res.data.items);
+      })
+
+      .catch((err) => {
+        console.log("youtube error: ", err);
+      });
+
+    // FoodTestAPI
+    const foodTest = async () => {
+      try {
+        // foodID 바꾸기
+        const response = await foodTestAPI(13, userEmail);
+        console.log("Test response: ", response.data.data);
+
+        setFoodData(response.data.data);
+      } catch (err) {
+        console.log("error: ", err);
+      }
+    };
+
+    useEffect(() => {
+      foodTest();
+    }, []);
+  };
+
+  const closeModal = (event) => {
+    event.stopPropagation();
+    setYoutubeData(null);
+    setFoodData(null);
+  };
+
   return (
     <div className="flex flex-col justify-center">
-      <img className="h-150 w-150 rounded-full" src={imgPath} alt="" />
-      <br />
-      <p className="text-lg text-center font-extrabold"> {foodTitle} </p>
+      {youtubeData && (
+        <Modal
+          close={closeModal}
+          searchResult={youtubeData}
+          food={foodData}
+        ></Modal>
+      )}
+      <button type="button" onClick={openModal}>
+        <img className="h-150 w-150 rounded-full" src={imgPath} alt="" />
+        <br />
+        <p className="text-lg text-center font-extrabold"> {foodTitle} </p>
+      </button>
     </div>
   );
 };
