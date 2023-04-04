d896008ffc926afd41963e21796dbbf720e65823
vinitus <tlsdnrng@gmail.com>
2023-04-04 21:29:53 +0900

    [feat] add the replace option to navigate

--- front/p-nut/src/hooks/useNavigateToTop.js
+++ front/p-nut/src/hooks/useNavigateToTop.js
@@ -3,9 +3,13 @@ import { useNavigate } from "react-router-dom";
 export const useNavigateToTop = () => {
   const navigate = useNavigate();
 
-  const navigateAndReset = (to) => {
-    navigate(to);
+  const navigateAndReset = (to, replace = false) => {
     window.scrollTo(0, 0);
+    if (replace) {
+      navigate(to, { replace });
+      return;
+    }
+    navigate(to);
   };
 
   return navigateAndReset;
