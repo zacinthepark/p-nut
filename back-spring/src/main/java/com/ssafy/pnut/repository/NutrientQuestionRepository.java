aa89a7d6cb1aac2fb921fa05012fadfd9adb5103
최재희 <withfavor98@naver.com>
2023-04-04 15:26:29 +0900

    fix:djangoapiuri

--- back-spring/src/main/java/com/ssafy/pnut/repository/NutrientQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.pnut.repository;
-
-import com.ssafy.pnut.entity.nutrientQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NutrientQuestionRepository extends JpaRepository<nutrientQuestion, Long> {
-}
