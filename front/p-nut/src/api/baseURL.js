60a1e5bb8932b49d16bb4ad2834e5cef1fbcc60e
Jinwoo Park <zacinthepark@gmail.com>
2023-04-06 12:16:50 +0900

    [fix] fix board list

--- front/p-nut/src/api/baseURL.js
+++ front/p-nut/src/api/baseURL.js
@@ -4,4 +4,7 @@ export const dataBaseURL = "https://pnut.site";
 
 export const imageBaseURL = "https://pnut.s3.ap-northeast-2.amazonaws.com";
 
+export const defaultProfileImageURL =
+  "basic_profile_image_37d8I092LMX89-removebg-preview.png";
+
 // export const baseURL = "http://j8a704.p.ssafy.io:9090/api";
