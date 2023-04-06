7462dcda1208c88010d94a41930f3c5df3853b22
강신욱 <tlsdnrng@gmail.com>
2023-04-06 12:32:16 +0900

    Update front/p-nut/src/api/baseURL.js

--- front/p-nut/src/api/baseURL.js
+++ front/p-nut/src/api/baseURL.js
@@ -1,7 +1,7 @@
 export const baseURL = "https://pnut.site/api";
 
-// export const dataBaseURL = "https://pnut.site";
-export const dataBaseURL = "http://j8a704.p.ssafy.io:8000";
+export const dataBaseURL = "https://pnut.site";
+// export const dataBaseURL = "http://j8a704.p.ssafy.io:8000";
 
 export const imageBaseURL = "https://pnut.s3.ap-northeast-2.amazonaws.com";
 
